2a62ba562550d58d040f650805e6d1093ee0c1ba
Andika <andikaprsty24@gmail.com>
2024-11-19 15:16:29 +0700

    p

--- inputdata.php
+++ inputdata.php
@@ -18,7 +18,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if ($conn->query($sql) === TRUE) {
         echo "
             <script>
-            alert('data berhasil ditambahkan!');
+            alert('data oramg hilang berhasil ditambahkan!');
             document.location.href='data.php';
             </script>
         ";
